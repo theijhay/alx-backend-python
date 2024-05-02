ce2dee56a8983babfa0e5389720a225dcac365bb
theijhay <olawaleisaacjohn@gmail.com>
2024-05-02 23:55:48 +0100

    type-annotated function make_multiplier that takes a float multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """type-annotated function make_multiplier that takes a float multiplier"""
+
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_func(num: float) -> float:
-        """multiply the input num by the multiplier"""
         return num * multiplier
+    """Returns the inner function"""
     return multiplier_func
