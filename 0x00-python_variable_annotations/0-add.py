e350f866c4b349d75881eb44f8d02062bf154efb
theijhay <olawaleisaacjohn@gmail.com>
2024-05-02 18:01:32 +0100

    type-annotated function add that takes a float a and a float b

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function add that takes a float a and a float b"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two integer togethers."""
+    return a + b
