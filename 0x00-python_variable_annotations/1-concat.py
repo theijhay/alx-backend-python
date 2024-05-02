be4bf6e73d1f72ad81a2db5522f754800c224564
theijhay <olawaleisaacjohn@gmail.com>
2024-05-02 18:13:46 +0100

    type-annotated function concat that takes a string str1 and a string str2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes a string str1 and a string str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Add the two parameters."""
+    return str1 + str2
