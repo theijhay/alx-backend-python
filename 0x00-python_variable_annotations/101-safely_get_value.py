8e1b8fd9dded5a888237eaa6ed802903e9f16ac5
theijhay <olawaleisaacjohn@gmail.com>
2024-05-03 01:01:10 +0100

    Given the parameters and the return values, add type annotations to the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,12 +6,9 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(
-    dct: Mapping[Any, T],
-    key: Any,
-    default: Union[T, None] = None
-) -> Union[Any, T]:
-    """Check and return the value"""
+def safely_get_value(dct: Mapping[Any, T], key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """Return either an object of any type or an object of the type"""
     if key in dct:
         return dct[key]
     else:
