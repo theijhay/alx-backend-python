9bd206581e7f3418cbaa4c6715bb3864869fd804
theijhay <olawaleisaacjohn@gmail.com>
2024-05-03 01:09:57 +0100

    Given the parameters and the return values, add type annotations to the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""Given the parameters and the return values,
-add type annotations to the function"""
-from typing import TypeVar, Mapping, Any, Union
+"""Add type annotations to the function"""
+from typing import Any, Mapping, Union, TypeVar
 
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    '''Gets the value safely
+    '''
     if key in dct:
         return dct[key]
     else:
