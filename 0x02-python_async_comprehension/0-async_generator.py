2428c7a8e1f3d5e4d2a0e61b8f0a583d12e9294a
theijhay <olawaleisaacjohn@gmail.com>
2024-05-07 14:15:45 +0100

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,6 +3,7 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
 async def async_generator():
@@ -13,12 +14,3 @@ async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
-
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-asyncio.run(print_yielded_values())
