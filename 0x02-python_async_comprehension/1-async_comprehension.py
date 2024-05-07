0127d6628ef470d0f886a81b27fe4266174a7f2f
theijhay <olawaleisaacjohn@gmail.com>
2024-05-07 15:16:49 +0100

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 
+
 import asyncio
 from typing import List
 from random import uniform
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_generator() -> List[float]:
